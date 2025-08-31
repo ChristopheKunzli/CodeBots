5bbcdbaa929e4c829ffa6db035c67de674584cd2
Schouwey <zaid.schouwey@gmail.com>
2025-08-31 18:18:39 +0200

    finish feature

--- src/client/world/items/item.ts
+++ src/client/world/items/item.ts
@@ -1,11 +1,5 @@
 import Tile from "../tile";
 
 export abstract class Item{
-    protected maxStackSize:number;
-
-    constructor(maxStackSize:number = 64){
-        this.maxStackSize = maxStackSize;
-    }
-
     public abstract use(tile:Tile):boolean;
 }
--- src/client/world/items/wood_item.ts
+++ src/client/world/items/wood_item.ts
@@ -2,7 +2,6 @@ import Tile from "../tile";
 import { Item } from "./item";
 
 export class WoodItem extends Item{
-    
     public use(tile: Tile): boolean {
         return false;
     }
