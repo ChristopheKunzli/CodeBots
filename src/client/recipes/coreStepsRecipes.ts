95d47a75085d84223746e4f4bc8b067148e97fa9
Christophe KUNZLI <christophe.kunzli@heig-vd.ch>
2025-09-02 19:27:13 +0200

    Better last core step

--- src/client/recipes/coreStepsRecipes.ts
+++ src/client/recipes/coreStepsRecipes.ts
@@ -4,6 +4,12 @@ import { StoneItem } from "../world/items/rawRessources/stone_item";
 import { CoalItem } from "../world/items/rawRessources/coal_item";
 import { IronIngotItem } from "../world/items/craft_ingredients/iron_ingot_item";
 import { CopperIngotItem } from "../world/items/craft_ingredients/copper_ingot_item";
+import { IronRodItem } from "../world/items/craft_ingredients/iron_rod_item";
+import { IronFrameItem } from "../world/items/craft_ingredients/iron_frame_item";
+import { IronPlateItem } from "../world/items/craft_ingredients/iron_plate_item";
+import { ReinforcedIronPlateItem } from "../world/items/craft_ingredients/reinforced_iron_plate_item";
+import { NailItem } from "../world/items/craft_ingredients/nail_item";
+import { CodeBotItem } from "../world/items/CodeBotItem";
 
 export const coreStepsRecipes: CoreStep[] = [
     {
@@ -36,9 +42,15 @@ export const coreStepsRecipes: CoreStep[] = [
         name: "Âge Moderne",
         items: [
             {item: new WoodLogItem(5000), currentGathered: 0},
-            {item: new CopperIngotItem(1500), currentGathered: 0},
-            {item: new IronIngotItem(1500), currentGathered: 0},
-            {item: new CoalItem(12000), currentGathered: 0},
+            {item: new CopperIngotItem(2000), currentGathered: 0},
+            {item: new IronIngotItem(2000), currentGathered: 0},
+            {item: new CoalItem(9000), currentGathered: 0},
+            {item: new IronRodItem(8000), currentGathered: 0},
+            {item: new IronFrameItem(8000), currentGathered: 0},
+            {item: new IronPlateItem(3000), currentGathered: 0},
+            {item: new ReinforcedIronPlateItem(1500), currentGathered: 0},
+            {item: new NailItem(12000), currentGathered: 0},
+            {item: new CodeBotItem(100), currentGathered: 0}
         ],
     }
 
