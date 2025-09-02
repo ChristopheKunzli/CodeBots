60ae9d8d4b2651c5798e080e08b8b948648148cd
Christophe KUNZLI <christophe.kunzli@heig-vd.ch>
2025-09-02 19:16:14 +0200

    Remove TODO

--- src/client/world/items/CodeBotItem.ts
+++ src/client/world/items/CodeBotItem.ts
@@ -1,5 +1,4 @@
 import { Item } from "./item";
-import { CraftingTable } from "../interactables/crafting_table";
 
 
 export class CodeBotItem extends Item{
@@ -9,12 +8,5 @@ export class CodeBotItem extends Item{
 
     public use(tile: any): boolean {
         return false;
-        //TODO place code bot
-        /*
-        if(tile.getContent != null)
-            return false;
-        tile.setContent = new CodeBot(tile);
-        return true;
-        */
     }
 }
