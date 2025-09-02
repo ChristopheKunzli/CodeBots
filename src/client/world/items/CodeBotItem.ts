608a5664543f30af8037624fd663197b1991130d
Christophe KUNZLI <christophe.kunzli@heig-vd.ch>
2025-09-02 19:19:11 +0200

    Add TODO back

--- src/client/world/items/CodeBotItem.ts
+++ src/client/world/items/CodeBotItem.ts
@@ -1,6 +1,5 @@
 import { Item } from "./item";
 
-
 export class CodeBotItem extends Item{
     constructor(quantity: number) {
         super("codebot_item", quantity);
@@ -8,5 +7,12 @@ export class CodeBotItem extends Item{
 
     public use(tile: any): boolean {
         return false;
+        //TODO place code bot
+        /*
+        if(tile.getContent != null)
+            return false;
+        tile.setContent = new CodeBot(tile);
+        return true;
+        */
     }
 }
